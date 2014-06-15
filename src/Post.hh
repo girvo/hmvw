7b550f9265536e6dbb15636decc6df43885633ac
Josh Girvin <josh@jgirvin.com>
2014-06-15 21:45:47 +1000

    Got update working, but need to check types

--- src/Post.hh
+++ src/Post.hh
@@ -1,6 +1,6 @@
 <?hh //strict
 
-newtype QueryData = ImmMap<string, mixed>;
+newtype QueryData = Map<string, mixed>;
 
 class Post
 {
@@ -10,7 +10,7 @@ class Post
         public int $id,
         public string $title,
         public string $body,
-        public DateTime $posted_at,
+        public DateTime $created_at,
         public int $author_id,
     ) {}
 }
@@ -25,15 +25,15 @@ class PostRepository
      * @param int $id The ID of the post to retrieve
      * @return ?Post The hydrated post model
      */
-    public function findOne(int $id): ?Post {
+    public function findOne(int $id): Post {
         
         $query = 'SELECT * FROM posts WHERE posts.id = :id LIMIT 1';
-        $query_data = ImmMap { ":id" => $id };
+        $query_data = Map { ":id" => $id };
         
         $result_set = $this->find($query, $query_data);
         
         if ($result_set->isEmpty()) {
-            return null;
+            //return null;
         }
         
         // get the (only) item
@@ -55,33 +55,62 @@ class PostRepository
      * @return bool Success or failure of update
      */
     public function update(Post $updated_post): bool {
-        // find the post first
-        $original_post_query = "SELECT id FROM posts WHERE posts.id = :id";
-        $original_post_data = ImmMap { ":id" => $updated_post->id };
+
+        $update_query = "UPDATE posts SET 
+                        title = :title, 
+                        body = :body,
+                        created_at = :created_at,
+                        author_id = :author_id
+                        WHERE `posts`.id = :id";
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        $result_set = $this->find($$original_post_query, $original_post_data);
-        
-        if ( $result_set->isEmpty() ) {
-            return false;
-            // throw exception?
-        }
-        
-        $original_post_id = $result_set[0];
+        $query = $this->db->prepare($update_query);
+        $data = $updated_post->asQueryData();
+
+        $query->execute($data);
+
+        var_dump($query->rowCount());
         
         return false;
     }
     
     public function doTest(): void {
-        $query = "SELECT id FROM posts WHERE posts.id = :id";
-        $data = ImmMap { ":id" => 1 };
-        $results_set = $this->find($query, $data);
+        $orig = $this->findOne(1);
         
-        var_dump($results_set[0]);
+        var_dump($orig);
+        
+        print "<br/><br/>";
+        
+        $new = $orig;
+        
+        $new->title = "This is an update test";
+        
+        $this->update($new);
+        
+        $updated = $this->findOne(1);
+        
+        var_dump($updated);
     }
+    
 }
 
 trait Model
 {
+    public function asQueryData(): Map<string, mixed> {
+        $result = Map {};
+        $props = get_object_vars($this);
+        
+        foreach ($props as $property => $value) {
+            // nasty :/
+            if ($value instanceof DateTime) {
+                $result[':'.$property] = $value->format('Y-m-d h:m:s');
+            } else {
+                $result[':'.$property] = $value;
+            }
+        }
+
+        return $result;
+    }
 }
 
 trait Repository
@@ -90,17 +119,24 @@ trait Repository
      * Private method to run particular queries for the public method calls
      * 
      * @param string $query The query string itself as a prepared statement
-     * @param ImmMap<string, string> $data Array of data to be passed into the query
+     * @param Map<string, string> $data Array of data to be passed into the query
      * @return Vector<Map<string, string>> The collection of results, can be empty
      */
+    private PDO $db;
+     
+    public function __construct(): void {
+        $this->db = new PDO('sqlite:../db/database.sqlite3');
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+     
     private function find(
           string $query,
           QueryData $data,
         ): Vector<Map<string, string>> {
     
-        $db = new PDO('sqlite:../db/database.sqlite3');
+        $this->db = new PDO('sqlite:../db/database.sqlite3');
         
-        $query = $db->prepare($query);
+        $query = $this->db->prepare($query);
         $query->execute($data);
         $result = $query->fetchAll();
         
@@ -111,23 +147,5 @@ trait Repository
         }
         
         return $real_result;
-    }
-    
-    /*private function runQuery(
-        
-        ): {
-        
-        $db = new PDO('sqlite:../db/database.sqlite3');
-        
-        $query = $db->prepare($query);
-        $query->execute($data);
-        $result = $query->fetchAll();
-    }*/
-    
-    /**
-     * Gets a count of rows that mataches a query
-     */
-    private function count(string $query, QueryData $data,): int {
-        
     }
 }
